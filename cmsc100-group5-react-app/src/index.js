1572c479c68394cbcf431476b8a5c227a1cfafaa
Janelle <jgmangalao@up.edu.ph>
2023-05-24 18:07:32 +0800

    feat: sign up page UI

--- cmsc100-group5-react-app/src/index.js
+++ cmsc100-group5-react-app/src/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Components
-import Root from './pages/Root';
-import Home from './pages/Home';
-import ViewSubmissions from './pages/ViewSubmissions';
+import Root from "./pages/Root";
+import Home from "./pages/Home";
+import ViewSubmissions from "./pages/ViewSubmissions";
+import SignUp from "./pages/SignUp";
 
 const router = createBrowserRouter([
   {
-    path: "/", element: <Root />, children: [
+    path: "/",
+    element: <Root />,
+    children: [
       { path: "/", element: <Home /> },
-      { path: "/view-submissions", element: <ViewSubmissions /> }
-    ]
-  }
-])
+      { path: "/view-submissions", element: <ViewSubmissions /> },
+      { path: "/sign-up", element: <SignUp /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
