09e8cf03c2d06afc2f303881de961f1fc8935776
Janelle <jgmangalao@up.edu.ph>
2023-05-24 13:09:23 +0800

    Added routing and styles for Home and View Submissions page.

--- cmsc100-group5-react-app/src/index.js
+++ cmsc100-group5-react-app/src/index.js
@@ -1,11 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Components
+import Root from './pages/Root';
+import Home from './pages/Home';
+import ViewSubmissions from './pages/ViewSubmissions';
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <Root />, children: [
+      { path: "/", element: <Home /> },
+      { path: "/view-submissions", element: <ViewSubmissions /> }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ cmsc100-group5-react-app/src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+
+  return (
+    <div className="homepage">
+      <h1 className="heading">Welcome!</h1>
+    </div>
+  )
+}
