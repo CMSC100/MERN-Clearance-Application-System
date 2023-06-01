9deb8812c4d5f8d5ae949be04f56cc9ece242ae9
rainpends <rppendon@up.edu.ph>
2023-06-01 13:30:23 +0800

    feat: Separated the header from the Root to this file. This header is for Student pages only.

---
+++ cmsc100-group5-react-app/src/components/StudentHeader.js
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { Button } from '@mui/material';
+import { Outlet, Link } from 'react-router-dom';
+
+export default function StudentHeader(props) {
+
+  return (
+    <>
+      <nav>
+        <ul>
+          <li><Link to={`/`} className="link-styles">Home</Link></li>
+          <li><Link to={`/view-submissions`} className="link-styles">View Clearance Applications</Link></li>
+        </ul>
+        <div className="user-icon-holder">
+          <div className="user-icon">
+            <Link to={`/profile`} props={props.onClick}><FontAwesomeIcon icon={icon({name: 'user-circle'})} className="icon user-icon"/></Link>
+          </div>
+        </div>
+        <Button
+                onClick={props.onClick}
+                variant="contained"
+                sx={{
+                bgcolor: "#001D3D",
+                borderRadius: 20,
+                margin: 0,
+                marginTop: 2,
+                '& .MuiButtonBase-root': {
+                  fontFamily: 'Poppins'
+                }
+                }}
+            >
+                Log Out
+            </Button>
+      </nav>
+    </>
+  )
+}
