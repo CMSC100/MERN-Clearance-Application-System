df6ef7b194e4fad047f9a0dd537b251c7c7b75ae
rainpends <rppendon@up.edu.ph>
2023-06-01 13:28:26 +0800

    chore: Added routes for getting the details of the current user and for getting all student accounts.

--- backend/routes.js
+++ backend/routes.js
@@ -1,10 +1,13 @@
 import { signUp, login, checkIfLoggedIn } from "./auth-controller.js";
+import { getUser, getStudentAccounts } from "./account-controller.js";
 
 const setUpRoutes = (app) => {
   app.get("/", (req, res) => { res.send("API Home") });
   app.post("/signup", signUp);
   app.post("/login", login);
   app.post("/checkifloggedin", checkIfLoggedIn);
+  app.get("/get-user", getUser)
+  app.get("/get-student-accounts", getStudentAccounts)
 }
 
 export default setUpRoutes;
