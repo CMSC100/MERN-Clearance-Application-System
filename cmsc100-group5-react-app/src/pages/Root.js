65571835317b7bf84428dc77e8ee443dec260b42
rainpends <rppendon@up.edu.ph>
2023-06-01 13:35:05 +0800

    chore: Removed the header components from this file.

--- cmsc100-group5-react-app/src/pages/Root.js
+++ cmsc100-group5-react-app/src/pages/Root.js
@@ -6,17 +6,6 @@ export default function Root() {
 
   return (
     <>
-      <nav>
-        <ul>
-          <li><Link to={`/`} className="link-styles">Home</Link></li>
-          <li><Link to={`/view-submissions`} className="link-styles">View Clearance Applications</Link></li>
-        </ul>
-        <div className="user-icon-holder">
-          <div className="user-icon">
-            <Link to={`/`}><FontAwesomeIcon icon={icon({name: 'user-circle'})} className="icon user-icon"/></Link>
-          </div>
-        </div>
-      </nav>
       <Outlet />
     </>
   )
