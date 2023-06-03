fd9df718069c79bcba5cf2b572ca39e593953543
rainpends <rppendon@up.edu.ph>
2023-06-04 01:17:27 +0800

    feat: Changed the look of the home page. Accounts now presented in table form.

--- cmsc100-group5-react-app/src/pages/AdminHome.js
+++ cmsc100-group5-react-app/src/pages/AdminHome.js
@@ -2,24 +2,74 @@ import React, { useEffect, useState } from "react"
 import { useNavigate, useLoaderData } from "react-router-dom"
 import AdminHeader from "../components/AdminHeader"
 import AccountCard from "../components/AccountCard";
+import { Link } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
 
 import Cookies from 'universal-cookie';
+import { Button } from "@mui/material";
+import { useRef } from "react";
 
-export default function AdminHome() {
-    const [accounts, setAccounts] = useState([])
-    const [isLoggedIn, setIsLoggedIn] = useState(useLoaderData())
-    const navigate = useNavigate()
+export default function AdminHome(props) {
+  const [accounts, setAccounts] = useState([])
+  const [isLoggedIn, setIsLoggedIn] = useState(useLoaderData())
+  const navigate = useNavigate()
 
-    useEffect(() => {
+  const [rows, setRows] = useState([])
+  const renderAfterCalled = useRef(false);
+
+  const columns = [
+    // {
+    //   field: 'id',
+    //   headerName: 'ID',
+    //   width: 70,
+    //   type: 'text'
+    // },
+    {
+      field: 'studentNo',
+      headerName: 'Student Number',
+      width: 200,
+      renderCell: (params) => (
+        <Link to={`/admin-home/${params.row.id}`}>{params.row.id}</Link>
+      )
+    },
+    {
+      field: 'studentName',
+      headerName: 'Student Name',
+      width: 400,
+      type: 'name'
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 200,
+      renderCell: (params) => (
+        <>
+          <Button className='approve-Btn' variant="contained" color="success" onClick={() => { approveAcc(params.row.id)}}>Approve</Button>
+          <Button className='reject-Btn' variant="contained" color="error" onClick={() => { rejectAcc(params.row.id)}}>Reject</Button>
+        </>
+        )
+    }
+  ]
+
+  useEffect(() => {
         if (!isLoggedIn) {
           navigate("/")
         } else {
-            fetch("http://localhost:3001/get-student-accounts").then(response => response.json())
-        .then(body => {
-            setAccounts(body)
-        })
-        }
-    }, [isLoggedIn, navigate])
+            fetch("http://localhost:3001/get-student-accounts")
+            .then(response => response.json())
+            .then(body => {
+              console.log(JSON.stringify(body))
+              console.log(body)
+              body.map((account)=>{
+                const newRow = {
+                  id: account.studentno,
+                  studentNumber: account.studentno,
+                  studentName: `${account.fname} ${account.lname}`,
+                }
+                setRows((oldRows)=>[...oldRows, newRow])
+            })}
+            )
+    }}, [isLoggedIn, navigate])
 
     function logout() {
         const cookies = new Cookies();
@@ -40,10 +90,12 @@ export default function AdminHome() {
         })
           .then(response => response.text())
           .then(body => {
-    
+            console.log(JSON.stringify(body))
             // check if Mongoose document was successfully edited/updated via the response returned
             if(Object.values(body)[0] ? "true" : "false" == "true") {
-              setAccounts(accounts.filter(items=>items.studentno!==studno))
+              // setAccounts(accounts.filter(items=>items.studentno!==studno))
+              setRows(rows.filter(row => row.id !== studno))
+              console.log(rows)
             }
           })
         }
@@ -62,18 +114,52 @@ export default function AdminHome() {
                 // when student account request is rejected, the user must then be deleted in the database
                 // check if Mongoose document was successfully deleted via the response returned
                 if(Object.values(body)[0] ? "true" : "false" == "true") {
-                    setAccounts(accounts.filter(items=>items.studentno!==studno))
+                    // setAccounts(accounts.filter(items=>items.studentno!==studno))
+                    setRows(rows.filter(row => row.id !== studno))
                 }
               })
             }
 
     return(
-        <>
-        
-        {isLoggedIn && <AdminHeader onClick={logout}/>}
-        <h2>Manage Student account applications</h2> 
-                {accounts.map((account, i) => <AccountCard num={i} account={account} onApprove={() => { approveAcc(account.studentno)}} onReject={() => { rejectAcc(account.studentno)}}></AccountCard>)}
-            
-        </>
+      <div className="homepage">
+        {isLoggedIn && <AdminHeader onClick={props.onClick}/>}
+        <h2 className="heading">Welcome, Admin {localStorage.getItem("username")}!</h2>
+        <h1 className="heading">Manage Student Account Applications</h1>
+        <div className="table">
+          <DataGrid
+                className='data-table'
+                sx={{
+                  fontFamily: 'Poppins',
+                  fontSize: '16px',
+                  color:"#001D3D;",
+                  '& .MuiToolbar-root *': {
+                    fontFamily: 'Poppins',
+                  },
+                  '& .MuiDataGrid-columnHeaders': {
+                    backgroundColor: "#001D3D",
+                    color: "white",
+                    fontWeight: "bold",
+                  },
+                  '& .MuiDataGrid-sortIcon': {
+                    color: "white",
+                  },
+                  '& .MuiDataGrid-menuIconButton': {
+                    color: "white",
+                  },
+                  '.MuiDataGrid-columnSeparator': {
+                    display: 'none',
+                  },
+                }}
+                rows={rows}
+                columns={columns}
+                initialState={{
+                  pagination: {
+                    paginationModel: { page: 0, pageSize: 5 },
+                  }
+                }}
+                pageSizeOptions={[5,10]}
+              />
+        </div>
+      </div>
     )
 }
